a1b65969e62e4a5ea2e85033c86c0d62658ecddb
daniyal <63174165+Daniyal-Abdul-Rehman@users.noreply.github.com>
2025-02-26 21:43:23 +0500

    update

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,7 +3,7 @@ import { boot } from 'quasar/wrappers';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/v1/',  // Replace with your API base URL
+  baseURL: 'https://backend-casino.vercel.app/v1/',  // Replace with your API base URL
   timeout: 10000,                        // Example timeout of 10 seconds
 });
 
